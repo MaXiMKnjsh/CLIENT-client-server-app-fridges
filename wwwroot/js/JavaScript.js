eba983cae7e48c836b584573a26a527107b70d3d
MaXi'm <maks.knysh.00@mail.ru>
2024-02-08 20:36:52 +0300

    recovered to working version

--- wwwroot/js/JavaScript.js
+++ wwwroot/js/JavaScript.js
@@ -1,37 +1,55 @@
 ﻿function openModal(params) {
     const url = params.url;
     const guid = params.guid;
+    const name = params.name;
     // селектор jQuery
-    const modal = $("#myModal");
-    const buttonOk = $("#buttonOk");
-    const buttonClose = $("#buttonClose");
+    const modal = $("#modal-confirmation");
 
     if (url === undefined || guid === undefined) {
         alert("Произошла ошибка!");
         return;
     }
 
-    modal.modal("show");
-}
-function removeObject() {
-    // метод jQuery для асинхронного http запроса к серверу
-    $.ajax({
-        type: "DELETE",
-        url: url,
-        data: { "guid": guid },
-        success:
-            function () {
-                modal.modal("hide");
-            },
-        //failure:
-        //    function () {
+    modal.find(".modal-body").html(`<p>Вы действительно хотите удалить ${name}?<p>`);
+    modal.modal('show');
 
-        //    },
-        error:
-            function () {
-            }
+    const buttonRemove = modal.find("#buttonRemove");
+    const buttonClose = modal.find("#buttonClose");
+    const buttonClose2 = modal.find("#buttonClose2");
+
+    buttonClose.click(function () {
+        closeModal(modal);
+    });
+
+    buttonClose2.click(function () {
+        closeModal(modal);
+    });
+
+    buttonRemove.click(function () {
+        removeObject(url, guid, modal);
     });
 }
-function closeModal() {
-    modal.modal("hide");
+
+function removeObject(url, guid, modal) {
+    //метод jQuery для асинхронного http запроса к серверу
+    $.ajax({
+        type: "DELETE",
+        url: url + "/" + guid,
+        success:
+            function (response) {
+                modal.modal("hide");
+                console.log(response);
+                location.reload();
+            },
+        error:
+            function (response) {
+                alert("Произошла ошибка при запросе к серверу!");
+                console.log(response);
+            }
+    });
+
+    closeModal(modal);
+}
+function closeModal(modal) {
+    $(modal).modal('hide');
 }
